c167f2a8d601ddd42d1c726e0f890783535a0dff
nkumanov <kumanov9696@gmail.com>
2025-04-22 13:39:37 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://weddinganienver.vercel.app/',
+    origin: 'https://weddinganienver.vercel.app',
     methods: 'GET,POST,PUT,DELETE',
   });
   await app.listen(process.env.PORT ?? (process.env.PORT || ''));
